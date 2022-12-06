141c74f78019dc9292e7d0860f004a8595b76335
Gavin Hope <ghope1@umbc.edu>
2022-12-06 14:06:05 -0500

    day1 committed slightly cleaned

--- day1.py
+++ day1.py
@@ -5,13 +5,12 @@ calorie_input = sys.stdin.read()
 calorie_list = calorie_input.split('\n')
 
 top_3 = [0, 0, 0]
-currMin = 0
 runningTotal = 0
 for calories in calorie_list:
     if calories == '':
-        top_3[top_3.index(currMin)] = runningTotal if runningTotal > currMin else currMin
-        runningTotal = 0
         currMin = min(top_3)
+        top_3[top_3.index(currMin)] = runningTotal if runningTotal > currMin else currMin
+        runningTotal = 0 
     else:
         runningTotal += int(calories)
 
